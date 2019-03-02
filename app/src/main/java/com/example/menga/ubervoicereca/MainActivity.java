4995e40c334fa38f91d46dcfd8957af758ecdc04
ame <33564712+ame@users.noreply.github.com>
2019-03-02 13:15:14 -0800

    driver and rider button on click

--- app/src/main/java/com/example/menga/ubervoicereca/MainActivity.java
+++ app/src/main/java/com/example/menga/ubervoicereca/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.menga.ubervoicereca;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                riderActivity();
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                driverActivity();
+            }
+        });
+    }
+    public void riderActivity(){
+        Intent intent = new Intent(this, rider.class);
+        startActivity(intent);
+    }
+    public void driverActivity(){
+        Intent intent = new Intent(this,driver.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/menga/ubervoicereca/rider.java
@@ -0,0 +1,13 @@
+package com.example.menga.ubervoicereca;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class rider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rider);
+    }
+}
